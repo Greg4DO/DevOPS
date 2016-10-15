fb4ea7565c17ae589fc06e3f39cd1e1f5c59fa8d
Greg <greg.ukr@gmail.com>
2016-10-15 11:44:09 +0300

    devops

---
+++ Linux/exam/DB/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+if [ ! -f /var/lib/mysql/ibdata1 ]; then
+
+mysql_install_db
+
+/usr/bin/mysqld_safe &
+sleep 10s
+echo "CREATE DATABASE 'jira'; GRANT ALL ON *.* TO admin@'%' IDENTIFIED BY 'jirapasswd' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+killall mysqld
+sleep 10s
+fi
+/usr/bin/mysqld_safe
